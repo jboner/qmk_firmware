0955a84fd338c0d4204f744856aa457aa4e3eca8
jboner <jonas@jonasboner.com>
2016-09-07 13:35:14 +0200

    Fixed error

--- keyboards/planck/keymaps/jboner/keymap.c
+++ keyboards/planck/keymaps/jboner/keymap.c
@@ -202,7 +202,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    _______, _______, KC_Y,   KC_U,  KC_I,    KC_O,    KC_P},
-  {KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_ENT,  KC_QUOT, KC_H,   KC_J,  KC_K,    KC_L,    KC_SEM},
+  {KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_ENT,  KC_QUOT, KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN},
   {KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_PGUP, KC_PGDN, KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH},
   {KC_LALT, KC_LCTL, KC_LGUI, LOWER, KC_LSFT, KC_LGUI, KC_TAB,  TC_SPC, RAISE, KC_VOLD, KC_VOLU, KC_MPLY}
 },
